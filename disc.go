9938a6b607ca298296fc427c6aae46a2b2292e58
Sean Williams <unixed@gmail.com>
2016-06-08 17:45:37 -0700

    Add support for detecting OSSEC files

--- disc.go
+++ disc.go
@@ -15,16 +15,78 @@ import (
 )
 
 var (
+	// CLI flags
 	do_gatt      = flag.Bool("gatt", false, "Get all the things")
 	do_container = flag.Bool("container", false, "Detect if we're in a container")
 	do_pkeys     = flag.Bool("pkeys", false, "Detect private keys")
+	do_av        = flag.Bool("av", false, "Check for signs of A/V services running or present")
 	pkeyDirs     = flag.String("pkeyDirs", "/root,/home", "Comma-separated dirs to search for private keys. Requires --pkeyDirs.")
 	pkeySleep    = flag.Int("pkeySleep", 0, "Length of time in milliseconds to sleep between examining files. Requires --pkeyDirs.")
+
+	//// Global config
+	// AV systems we can detect
+	AVSystems = []AVDiscoverer{
+		OSSECAV{name: "OSSEC"},
+	}
+	// AV system processes that indicate presence
+	AVSystemProcs = map[string][]string{
+		"OSSEC": []string{
+			"ossec-agentd",
+			"ossec-syscheckd",
+		},
+	}
 )
 
 type privateKey struct {
 	path      string
 	encrypted bool
+}
+
+type process struct {
+	pid  int
+	name string
+}
+
+// AV object holds the AV system name and
+type OSSECAV struct {
+	AVDiscoverer
+	// name of the AV system
+	name string
+}
+
+type AVDiscoverer interface {
+	Paths() []string
+	Procs() []process
+	Name() string
+}
+
+// AVResult holds information about AV systems present and/or running.
+type AVResult struct {
+	// Name of the detected AV system
+	name string
+	// filesystem paths of binaries related to the detected system
+	paths []string
+	// running processes related to the detected system
+	procs []string
+}
+
+func (o OSSECAV) Paths() []string {
+	paths := []string{
+		"/var/ossec",
+	}
+	found := []string{}
+	for _, path := range paths {
+		if _, err := os.Stat(path); err == nil {
+			found = append(found, path)
+		}
+	}
+	return found
+}
+func (o OSSECAV) Procs() []process {
+	return nil
+}
+func (o OSSECAV) Name() string {
+	return o.name
 }
 
 func getPrivateKey(path string) privateKey {
@@ -56,9 +118,9 @@ func getPrivateKey(path string) privateKey {
 	return p
 }
 
-// sshKeys looks for readable ssh private keys. Optionally sleep for `sleep`
+// getSSHKeys looks for readable ssh private keys. Optionally sleep for `sleep`
 // milliseconds to evade detection.
-func sshKeys(dir string, sleep int) []privateKey {
+func getSSHKeys(dir string, sleep int) []privateKey {
 	pkeys := []privateKey{}
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if sleep != 0 {
@@ -102,6 +164,18 @@ func isContainer() bool {
 	}
 	return false
 }
+
+func getAV() []AVDiscoverer {
+	allAV := []AVDiscoverer{}
+	for _, av := range AVSystems {
+		allAV = append(allAV, av)
+	}
+	return allAV
+}
+
+// TODO:
+// OSSEC process: ossec-agentd,ossec-syscheckd; OR just check for processes starting with "ossec-"?
+// Sophos process: sav-scan(?)
 func main() {
 	flag.Parse()
 	if *do_gatt || *do_container {
@@ -110,8 +184,18 @@ func main() {
 	if *do_gatt || *do_pkeys {
 		fmt.Printf("ssh keys:")
 		for _, dir := range strings.Split(*pkeyDirs, ",") {
-			for _, pkey := range sshKeys(dir, *pkeySleep) {
+			for _, pkey := range getSSHKeys(dir, *pkeySleep) {
 				fmt.Printf("\n\tfile=%v encrypted=%v", pkey.path, pkey.encrypted)
+			}
+		}
+		fmt.Println("")
+	}
+	if *do_av {
+		fmt.Printf("AV:")
+		for _, av := range getAV() {
+			name, paths, procs := av.Name(), av.Paths(), av.Procs()
+			if len(paths) > 0 || len(procs) > 0 {
+				fmt.Printf("\n\tname=%s files=%v procs=%v", name, paths, procs)
 			}
 		}
 		fmt.Println("")
